b196ca305c1964af98ac813ae07ab17f42e99c0d
Sabeel Ahmad <sabeelbasharat@gmail.com>
2019-04-30 22:51:14 +0530

    Updates

---
+++ precache-manifest.d942425317af9a802e7c47699a094b6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32d9d242f163d70bb330",
+    "url": "/blogit/static/js/runtime~main.60e45ad6.js"
+  },
+  {
+    "revision": "86946ce6e99406d355da",
+    "url": "/blogit/static/js/main.5d0a9cd4.chunk.js"
+  },
+  {
+    "revision": "939757f6df02ccfbcb99",
+    "url": "/blogit/static/js/2.85d61371.chunk.js"
+  },
+  {
+    "revision": "86946ce6e99406d355da",
+    "url": "/blogit/static/css/main.dc5bd4ae.chunk.css"
+  },
+  {
+    "revision": "c1230607dbc04a63df4f7b103d2eedf7",
+    "url": "/blogit/index.html"
+  }
+];
